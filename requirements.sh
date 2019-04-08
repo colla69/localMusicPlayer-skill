d09667cca3d8702addf302fceb1b126e868afb93
Andrea Colarieti Tosti <colla69@gmail.com>
2019-04-08 09:30:14 +0200

    new requirement.sh

--- requirements.sh
+++ requirements.sh
@@ -9,30 +9,32 @@
 # Uncomment the code first by removing the pound `#` signs in front of each line. 
 ##########################
 
-# detect distribution using lsb_release (may be replaced parsing /etc/*release)
-# dist=$(lsb_release -ds)
+#detect distribution using lsb_release (may be replaced parsing /etc/*release)
+dist=$(lsb_release -ds)
+
+dependencies=( scree, cmus )
 
 # setting dependencies and package manager in relation to the distribution
-#  if $(hash pkcon 2>/dev/null); then
-#     pm="pkcon"
-# else
-#     priv="sudo"
-#     if [ "$dist"  == "\"Arch Linux\""  ]; then
-#         pm="pacman -S"
-#         dependencies=( aaa )
-#     elif [[ "$dist" =~  "Ubuntu" ]] || [[ "$dist" =~ "Debian" ]] ||[[ "$dist" =~ "Raspbian" ]]; then
-#         pm="apt install"
-#         dependencies=( bbb)
-#     elif [[ "$dist" =~ "SUSE" ]]; then 
-#         pm="zypper install"
-#         dependencies=( ccc )
-#     fi
-# fi
+  if $(hash pkcon 2>/dev/null); then
+     pm="pkcon"
+ else
+     priv="sudo"
+     if [ "$dist"  == "\"Arch Linux\""  ]; then
+         pm="pacman -S"
+         dependencies=( aaa )
+     elif [[ "$dist" =~  "Ubuntu" ]] || [[ "$dist" =~ "Debian" ]] ||[[ "$dist" =~ "Raspbian" ]]; then
+         pm="apt install"
+         dependencies=( bbb)
+     elif [[ "$dist" =~ "SUSE" ]]; then
+         pm="zypper install"
+         dependencies=( ccc )
+     fi
+ fi
 
 # installing dependencies
-# if [ ! -z "$pm" ]; then
-#     for dep in "${dependencies[@]}"
-#    do
-#         $priv $pm $dep
-#    done
-# fi
+ if [ ! -z "$pm" ]; then
+     for dep in "${dependencies[@]}"
+    do
+         $priv $pm $dep
+    done
+ fi
